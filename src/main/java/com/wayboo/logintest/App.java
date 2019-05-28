df7c1581a34c1b025abb37d7396efd1203c2c006
养码哥 <ithelei@sina.cn>
2019-05-28 22:20:50 +0800

    测试

--- src/main/java/com/wayboo/logintest/App.java
+++ src/main/java/com/wayboo/logintest/App.java
@@ -3,15 +3,7 @@ package com.wayboo.logintest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @Author dlei(徐磊)
- * @Tel  18665616520
- * @Email dlei0009@163.com
- * @QQ 251425887
- * @Date 2017年7月23日下午8:06:42
- * @From http://www.fkjava.org 疯狂软件
- * @Version 1.0
- */
+
 // @SpringBootApplication指定这是一个 spring boot的应用程序.
 @SpringBootApplication
 public class App 
